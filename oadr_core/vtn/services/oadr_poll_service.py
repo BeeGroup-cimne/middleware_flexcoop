0ef607a66a4df656d5cc88951a84e4577a5131fb
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2019-10-11 17:02:48 +0200

    corrected error in oadr_poll

--- oadr_core/vtn/services/oadr_poll_service.py
+++ oadr_core/vtn/services/oadr_poll_service.py
@@ -1,13 +1,15 @@
 from oadr_core.oadr_base_service import OadrMessage
+from project_customization.flexcoop.models import oadrPollQueue
 from oadr_core.oadr_payloads.oadr_payloads_general import NAMESPACES, oadrResponse, oadrPayload
 
 
 class OadrPoll(OadrMessage):
-    global oadrPollQueue
     def _create_response(self, params):
         final_parameters = params.xpath(".//oadr:oadrPoll", namespaces=NAMESPACES)[0]
         venID = final_parameters.find(".//ei:venID", namespaces=NAMESPACES).text
+        print("AAAAA")
         if venID in oadrPollQueue and oadrPollQueue[venID]:
+            print(venID)
             type, response = oadrPollQueue[venID].pop(0)
         else:
             response = oadrPayload(oadrResponse("200", "OK", "0", venID))
