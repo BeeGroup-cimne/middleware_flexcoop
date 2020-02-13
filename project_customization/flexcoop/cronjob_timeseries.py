75b0d999b25e28fd30ddebecaa02219a247549b9
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2020-02-13 18:56:02 +0100

    cronjobs correction

--- project_customization/flexcoop/cronjob_timeseries.py
+++ project_customization/flexcoop/cronjob_timeseries.py
@@ -1,13 +1,13 @@
 from datetime import datetime, timedelta
 import sys
 
-from pymongo import UpdateOne, ReplaceOne
+from pymongo import UpdateOne, ReplaceOne, DeleteMany
 
 sys.path.extend([sys.argv[1]])
 from mongo_orm import MongoDB, AnyField
 from project_customization.flexcoop.models import DataPoint
 from project_customization.flexcoop.reports.telemetry_usage import get_data_model
-from project_customization.flexcoop.timeseries_utils import timeseries_mapping
+from project_customization.flexcoop.timeseries_utils import timeseries_mapping, indoor_sensing, occupancy, meter
 
 import pandas as pd
 """We define the cronjobs to be executed to deal with the raw data recieved"""
@@ -16,58 +16,137 @@ import pandas as pd
 
 
 def aggregate_timeseries(freq):
+    #search for all reporting devices
+    devices = set()
+    indoor_sensing_raw_data = []
+    occupancy_raw_data = []
+    meter_raw_data = []
     for key, value in timeseries_mapping.items():
         raw_model = get_data_model(key)
-        data = MongoDB.to_dict(raw_model.find({}))
-        if not data:
-            continue
-        df = pd.DataFrame.from_records(data)
-        print(key)
-        for device, data_device in df.groupby("device_id"):
-            print(device)
+        devices.update(raw_model.__mongo__.distinct("device_id"))
+        if value['class'] == indoor_sensing:
+            indoor_sensing_raw_data.append(key)
+        elif value['class'] == occupancy:
+            occupancy_raw_data.append(key)
+        elif value['class'] == meter:
+            meter_raw_data.append(key)
+    #iterate for each device to obtain the clean data of each type.
+    for device in devices:
+        indoor_sensing_df = []
+        occupancy_df = []
+        meter_df = []
+        print(device)
+        for key, value in timeseries_mapping.items():
+            raw_model = get_data_model(key)
+            data = MongoDB.to_dict(raw_model.find({"device_id": device}))
+            if not data:
+                continue
+            print(key)
+            df = pd.DataFrame.from_records(data)
             # get the data_point information
             point = DataPoint.find_one({"device_id": device})
             if not key in point.reporting_items:
                 continue
             point_info = point.reporting_items[key]
             reading_type = point_info['reading_type']
-            data_device.index = pd.to_datetime(data_device.dtstart)
-            account_id = data_device.account_id.unique()[0]
-            aggregator_id = data_device.aggregator_id.unique()[0]
+            df.index = pd.to_datetime(df.dtstart)
+            df = df.sort_index()
+            account_id = df.account_id.unique()[0]
+            aggregator_id = df.aggregator_id.unique()[0]
             device_class = point.rid
             print("readed data")
             if value['operation'] == "AVG":
-                data_device.value = pd.to_numeric(data_device.value)
+                df.value = pd.to_numeric(df.value)
                 if reading_type == "Direct Read":  # accumulated
-                    data_device.value = data_device.value.diff()
-                data_clean = data_device[['value']].resample("1s").mean().interpolate().resample(freq).mean()
+                    df.value = df.value.diff()
+                data_clean = df[['value']].resample("1s").mean().interpolate().resample(freq).mean()
             elif value['operation'] == "FIRST":
                 try:
                     if reading_type == "Direct Read":  # accumulated
-                        data_device.value = data_device.value.diff()
+                        df.value = df.value.diff()
                 except:
                     pass
-                data_clean = data_device[['value']].resample(freq).first()
+                data_clean = df[['value']].resample(freq).first()
             elif value['operation'] == "MAX":
-                data_device.value = pd.to_numeric(data_device.value)
+                df.value = pd.to_numeric(df.value)
                 if reading_type == "Direct Read":  # accumulated
-                    data_device.value = data_device.value.diff()
-                data_clean = data_device[['value']].resample(freq).max()
+                    df.value = df.value.diff()
+                data_clean = df[['value']].resample(freq).max()
             elif value['operation'] == "SUM":
-                data_device.value = pd.to_numeric(data_device.value)
+                df.value = pd.to_numeric(df.value)
                 if reading_type == "Direct Read":  # accumulated
-                    data_device.value = data_device.value.diff()
-                data_clean = data_device[['value']].cumsum().resample("1s").mean().interpolate().resample(freq).mean().diff()
+                    df.value = df.value.diff()
+                data_clean = df[['value']].cumsum().resample("1s").mean().interpolate().resample(freq).mean().diff()
             else:
                 data_clean = pd.DataFrame()
 
-            print("treated data")
-            bulk_write = []
-            print("writting_data {}".format(data_clean.count().value))
-            for ts, v in data_clean.iterrows():
-                params = {'account_id': account_id, 'aggregator_id': aggregator_id, "device_id": device, "device_class": device_class, "timestamp": ts, value['field']: v.value}
-                bulk_write.append(ReplaceOne({"device_id": device, "timestamp": ts}, params, True))
-            raw_model.__mongo__.bulk_write(bulk_write)
+            if data_clean.empty:
+                continue
+            data_clean[value['field']] = data_clean['value']
+            data_clean = data_clean.drop("value", axis=1)
+            if key in indoor_sensing_raw_data:
+                indoor_sensing_df.append(data_clean)
+            elif key in occupancy_raw_data:
+                occupancy_df.append(data_clean)
+            elif key in meter_raw_data:
+                meter_df.append(data_clean)
+            else:
+                continue
+        print("treated data")
+        # join all df and save them to mongo.
+
+        if indoor_sensing_df:
+            indoor_sensing_final = indoor_sensing_df.pop(0)
+            indoor_sensing_final = indoor_sensing_final.join(indoor_sensing_df)
+            indoor_sensing_final['account_id'] = account_id
+            indoor_sensing_final['aggregator_id'] = aggregator_id
+            indoor_sensing_final['device_class'] = device_class
+            indoor_sensing_final['device_id'] = device
+            indoor_sensing_final['timestamp'] = indoor_sensing_final.index.to_pydatetime()
+            indoor_sensing_final['_created_at'] = datetime.utcnow()
+            indoor_sensing_final['_updated_at'] = datetime.utcnow()
+            df_ini = min(indoor_sensing_final.index)
+            df_max = max(indoor_sensing_final.index)
+            documents = indoor_sensing_final.to_dict('records')
+            print("writting_sensing_data {}".format(len(documents)))
+            indoor_sensing.__mongo__.delete_many({"device_id": device, "timestamp": {"$gte":df_ini.to_pydatetime(), "$lte": df_max.to_pydatetime()}})
+            indoor_sensing.__mongo__.insert_many(documents)
+
+        if occupancy_df:
+            occupancy_final = occupancy_df.pop(0)
+            occupancy_final = occupancy_final.join(occupancy_df)
+            occupancy_final['account_id'] = account_id
+            occupancy_final['aggregator_id'] = aggregator_id
+            occupancy_final['device_class'] = device_class
+            occupancy_final['device_id'] = device
+            occupancy_final['timestamp'] = occupancy_final.index.to_pydatetime()
+            occupancy_final['_created_at'] = datetime.utcnow()
+            occupancy_final['_updated_at'] = datetime.utcnow()
+            df_ini = min(occupancy_final.index)
+            df_max = max(occupancy_final.index)
+            documents = occupancy_final.to_dict('records')
+            print("writting_occupancy_data {}".format(len(documents)))
+            occupancy.__mongo__.delete_many(
+                {"device_id": device, "timestamp": {"$gte": df_ini.to_pydatetime(), "$lte": df_max.to_pydatetime()}})
+            occupancy.__mongo__.insert_many(documents)
+
+        if meter_df:
+            meter_final = meter_df.pop(0)
+            meter_final = meter_final.join(meter_df)
+            meter_final['account_id'] = account_id
+            meter_final['aggregator_id'] = aggregator_id
+            meter_final['device_class'] = device_class
+            meter_final['device_id'] = device
+            meter_final['timestamp'] = meter_final.index.to_pydatetime()
+            meter_final['_created_at'] = datetime.utcnow()
+            meter_final['_updated_at'] = datetime.utcnow()
+            df_ini = min(meter_final.index)
+            df_max = max(meter_final.index)
+            documents = meter_final.to_dict('records')
+            print("writting_meter_data {}".format(len(documents)))
+            meter.__mongo__.delete_many(
+                {"device_id": device, "timestamp": {"$gte": df_ini.to_pydatetime(), "$lte": df_max.to_pydatetime()}})
+            meter.__mongo__.insert_many(documents)
 
 # Call this function everyday at 00:00
 def delete_raw_data():
