3b42175c504e90a8ca8df883c080a1a1a661768c
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2020-04-01 17:04:30 +0200

    debug in updateReports

--- oadr_core/vtn/server_blueprint.py
+++ oadr_core/vtn/server_blueprint.py
@@ -80,8 +80,13 @@ def openADR_VTN_service(service):
             response = etree.tostring(responder.respond(payload))
             if 'recieve' in events:
                 app.response_callback.append((events['recieve'], response))
-            if message == "oadrPoll":
-                print(response)
+            if message == "oadrUpdateReport":
+                try:
+                    value = payload.xpath("//rid", namespaces=NAMESPACES)
+                    print(value)
+                except:
+                    print("notvalid rid query")
+
             return response, 200, {'Content-Type': 'text/xml; charset=utf-8'}
         except KeyError as e:
             abort(Response("The message {} can't be found for this service {}".format(message, service), 400))
